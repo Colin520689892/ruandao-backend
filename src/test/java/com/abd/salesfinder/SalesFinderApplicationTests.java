900481735eed2941061553c31c3fcba92a0c43f6
Colin520689892 <1485940063@qq.com>
2024-09-19 13:24:43 +0800

    初始项目

---
+++ src/test/java/com/abd/salesfinder/SalesFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abd.salesfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
